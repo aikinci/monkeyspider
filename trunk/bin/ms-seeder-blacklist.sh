1e98b2a29a98990915c6bbb5eb71e75b56727ee4
riker2000 <riker2000@17611398-2c96-40f3-8e04-745c0424d2c9>
2009-02-26 15:11:22 +0000

    Updated the blacklist seeder. It works now for all hosts lists providers and needs unzip and p7zip to extract the files.

--- trunk/bin/ms-seeder-blacklist.sh
+++ trunk/bin/ms-seeder-blacklist.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #$Id$
 
-# ms-seeder-blacklist - load hosts files from several places of the internet and 
+# ms-seeder-blacklist.sh - load hosts files from several places of the internet and 
 #                    add them to one urllist
 
 # Copyright (C) 2006-2008 Ali Ikinci (ali at ikinci dot info)
@@ -21,33 +21,62 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with the Monkey-Spider project.  If not, see <http://www.gnu.org/licenses/>.
+echo This script downloads and concatenates hosts from various hosts file providers
+# Check if required programs are installed
+echo Checking installed programs first
+for CMD in awk sed wget unzip grep p7zip; do
+   type $CMD &> /dev/null
+   if [ $? != "0" ]; then
+      echo "The command '$CMD' is required and is not in your path."
+      exit 1
+   fi
+done
+
+CURDIR=$(pwd)
+#echo $CURDIR
+cd /tmp
+
+# Delete any hosts file and temporary directory which might exist previously
+rm -rf hosts*
+
+touch hosts 
 
 #### http://hostsfile.mine.nu
-wget http://hostsfile.mine.nu.nyud.net:8080/Hosts.zip -O hosts.tmp.zip
-unzip -f hosts.tmp.zip
+wget http://hostsfile.mine.nu/Hosts.zip -O hosts.tmp.zip
+unzip hosts.tmp.zip
 cat Hosts >> hosts
 rm Hosts hosts.tmp.zip
+
 #### http://www.mvps.org/winhelp2002/hosts.htm
-wget http://www.mvps.org/winhelp2002/hosts.txt -O hosts.tmp
-cat hosts.tmp >> hosts
-rm hosts.tmp
-#### http://www.hostsfile.org/hosts.html (they have changed their file format to 7zip)
-# wget http://www.hostsfile.org/BadHosts.tar.gz
-# tar xfz BadHosts.tar.gz
-# cat BadHosts/hosts.lnx >> hosts
-# rm -rf BadHosts*
-#### http://hphosts.mysteryfcm.co.uk/?s=Download
-wget http://hphosts.mysteryfcm.co.uk/download/hosts.zip -O hosts.tmp.zip
-unzip -f hosts.tmp.zip -d hoststmp
-cat hoststmp/HOSTS.TXT >> hosts
+wget http://www.mvps.org/winhelp2002/hosts.zip -O hosts.tmp.zip
+unzip hosts.tmp.zip -d hoststmp
+cat hoststmp/HOSTS >> hosts
 rm -rf hoststmp hosts.tmp.zip
+
+#### http://www.hostsfile.org/hosts.html
+wget http://www.hostsfile.org/Downloads/BadHosts.unx.7z -O hosts.tmp.7z
+p7zip -d hosts.tmp.7z
+find BadHosts.unx -iname "add.*" -exec cat {} >> hosts \;
+rm -rf hosts.tmp.7z BadHosts.unx
+
+#### http://hosts-file.net/
+wget http://hosts-file.net/download/hphosts.zip -O hosts.tmp.zip
+unzip hosts.tmp.zip -d hoststmp
+cat hoststmp/HOSTS.txt >> hosts
+rm -rf hoststmp hosts.tmp.zip
+
 #### http://someonewhocares.org/hosts/
 wget http://someonewhocares.org/hosts/hosts -O hosts.tmp
 cat hosts.tmp >> hosts
-#### http://www.everythingisnt.com/hosts.html
-wget http://everythingisnt.com/hosts -O hosts.tmp
-cat hosts.tmp >> hosts
+rm hosts.tmp
+
 #### filter and format the file
 grep -v '#' hosts |sed '/^$/d' | awk '{print $2}'|sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/\x0D$//'| sort -u   > hosts.tmp
-mv hosts.tmp hosts
+rm hosts
+
+cd $CURDIR
+mv /tmp/hosts.tmp hosts
+
+echo Gathered $(wc -l hosts |awk '{print $1}') unique hosts and wrote it to the file hosts
+
 
